87f9ee52e1954efdc1e5dce2e263c3eb8d69a0c6
matias fuentes <matias.fuentes84@gmail.com>
2024-09-06 11:48:09 -0400

    PrimeraEntregaBE2-3

--- src/utils.js
+++ src/utils.js
@@ -2,22 +2,26 @@ import bcrypt from "bcrypt";
 import passport from "passport";
 
 export const passportCall = (estrategia) => (req, res, next) => {
-  passport.authenticate(estrategia, (error, usuario, info) => {
-    if (error) {
-      return next(error);
-    } // contempla salidas return done(error) de la estrategia
+  passport.authenticate(
+    estrategia,
+    { session: false },
+    (error, usuario, info) => {
+      if (error) {
+        return next(error);
+      } // contempla salidas return done(error) de la estrategia
 
-    if (!usuario) {
-      // contempla salidas return done(null, false) de la estrategia
-      res.setHeader("Content-Type", "application/json");
-      return res
-        .status(400)
-        .json({ error: `${info.message ? info.message : info.toString()}` });
+      if (!usuario) {
+        // contempla salidas return done(null, false) de la estrategia
+        res.setHeader("Content-Type", "application/json");
+        return res
+          .status(400)
+          .json({ error: `${info.message ? info.message : info.toString()}` });
+      }
+
+      req.user = usuario; // contempla salidas return done(null, usuario) de la estrategia
+      next();
     }
-
-    req.user = usuario; // contempla salidas return done(null, usuario) de la estrategia
-    next();
-  })(req, res, next);
+  )(req, res, next);
 };
 
 export const generaHash = (password) =>
